e7d9f14f459b920aa25e3fd1567e3dac7e3021aa
Devashish.Gupta <devashish.gupta@inmobi.com>
2013-04-10 14:29:13 +0530

    Chnages

--- Server/src/main/java/com/inmobi/adserve/channels/server/RequestParser.java
+++ Server/src/main/java/com/inmobi/adserve/channels/server/RequestParser.java
@@ -203,6 +203,9 @@ public class RequestParser {
       }
       String uid = stringify(userIdMap, "u-id", logger);
       parameter.uid = (uid != null ? uid : stringify(userIdMap, "UDID", logger));
+      if(parameter.uid != null && parameter.uid.length() != 32) {
+        parameter.uid = MD5(parameter.uid);
+      }
       parameter.uidO1 = stringify(userIdMap, "O1", logger);
       parameter.uidMd5 = stringify(userIdMap, "UM5", logger);
       parameter.uidIFA = stringify(userIdMap, "IDA", logger);;
@@ -214,5 +217,19 @@ public class RequestParser {
       logger.info("Error in extracting userid params");
     }
   }
+  
+  public static String MD5(String md5) {
+    try {
+         java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+         byte[] array = md.digest(md5.getBytes());
+         StringBuffer sb = new StringBuffer();
+         for (int i = 0; i < array.length; ++i) {
+           sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+        }
+         return sb.toString();
+     } catch (java.security.NoSuchAlgorithmException e) {
+     }
+     return null;
+ }
 
 }
