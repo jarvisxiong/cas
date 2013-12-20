c069df271dbceb24643b83f4d90024f0131a0a6b
abhishek parwal <abhishek.parwal@inmobi.com>
2013-12-20 19:58:18 +0530

    netty server modularized, request logging standardized,  servlet handling

---
+++ Server/src/main/java/com/inmobi/adserve/channels/server/LoggerConfiguration.java
@@ -0,0 +1,17 @@
+package com.inmobi.adserve.channels.server;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Target;
+
+
+/**
+ * @author abhishek.parwal
+ * 
+ */
+@Target({ FIELD, METHOD, PARAMETER })
+@interface LoggerConfiguration {
+
+}
---
+++ Server/src/main/java/com/inmobi/adserve/channels/server/RequestIdHandler.java
@@ -0,0 +1,32 @@
+package com.inmobi.adserve.channels.server;
+
+import java.util.Random;
+
+import org.jboss.netty.channel.ChannelEvent;
+import org.jboss.netty.channel.ChannelHandler.Sharable;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.slf4j.MDC;
+
+
+/**
+ * @author abhishek.parwal
+ * 
+ */
+@Sharable
+public class RequestIdHandler extends SimpleChannelUpstreamHandler {
+
+    private final Random randomNumberGenerator;
+
+    public RequestIdHandler() {
+        randomNumberGenerator = new Random();
+    }
+
+    @Override
+    public void handleUpstream(final ChannelHandlerContext ctx, final ChannelEvent e) throws Exception {
+
+        MDC.put("requestId", System.currentTimeMillis() + "-" + randomNumberGenerator.nextInt(99999999));
+        super.handleUpstream(ctx, e);
+    }
+
+}
