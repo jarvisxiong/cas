eded00ded9b156fa05ec11947dabbc23a305668e
abhishek parwal <abhishek.parwal@inmobi.com>
2014-02-19 20:50:54 +0530

    filter to drop the segment , ( if request is from rtb OR segment belongs to rtb , and if adapter is not rtb's or rtb is disabled ) || (if segment is dcp , and if adapter is not dcp) - this filter will drop all such invalid request

--- Server/src/main/java/com/inmobi/adserve/channels/server/RequestIdHandler.java
+++ Server/src/main/java/com/inmobi/adserve/channels/server/RequestIdHandler.java
@@ -1,7 +1,5 @@
 package com.inmobi.adserve.channels.server;
 
-import java.util.Random;
-
 import org.jboss.netty.channel.ChannelHandler.Sharable;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
@@ -16,23 +14,10 @@ import org.slf4j.MDC;
 @Sharable
 public class RequestIdHandler extends SimpleChannelUpstreamHandler {
 
-    private final Random randomNumberGenerator;
-
-    public RequestIdHandler() {
-        randomNumberGenerator = new Random();
-    }
-
     @Override
     public void messageReceived(final ChannelHandlerContext ctx, final MessageEvent e) throws Exception {
         MDC.put("requestId", e.getChannel().getId().toString());
         super.messageReceived(ctx, e);
     }
 
-    // @Override
-    // public void handleUpstream(final ChannelHandlerContext ctx, final ChannelEvent e) throws Exception {
-    //
-    // MDC.put("requestId",
-    // String.format("%s-%s", System.currentTimeMillis(), randomNumberGenerator.nextInt(99999999)));
-    // super.handleUpstream(ctx, e);
-    // }
 }
