f9558055f0f8558d2006edc4a013aa64ba08206c
ritwik.kumar <ritwik.kumar@inmobi.com>
2014-07-01 17:53:45 +0530

    wap site uac function added to avoid blank rows

--- integration-repository/sql/wap_site_uac_type.sql
+++ integration-repository/sql/wap_site_uac_type.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE TYPE wap_site_uac_type AS
     site_type_id            bigint,
     content_rating          character varying(255),
     app_type                character varying(255),
+    categories              text,
     uac_mod_on              TIMESTAMP WITHOUT TIME ZONE
 );
 ALTER TYPE wap_site_uac_type OWNER TO postgres;
