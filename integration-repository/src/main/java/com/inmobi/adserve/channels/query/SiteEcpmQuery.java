888db9973c88f2dd69397b102518776e5549c1ca
ritwik.kumar <ritwik.kumar@inmobi.com>
2014-07-03 18:39:37 +0530

    adding to string to key

--- integration-repository/src/main/java/com/inmobi/adserve/channels/query/SiteEcpmQuery.java
+++ integration-repository/src/main/java/com/inmobi/adserve/channels/query/SiteEcpmQuery.java
@@ -1,14 +1,17 @@
 package com.inmobi.adserve.channels.query;
 
 import com.inmobi.phoenix.data.RepositoryQuery;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class SiteEcpmQuery implements RepositoryQuery {
 
     private String  siteId;
