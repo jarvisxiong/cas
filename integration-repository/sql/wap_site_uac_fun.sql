86db3a4856411aec62ef119bb85a00fe31fe31c3
ritwik.kumar <ritwik.kumar@inmobi.com>
2014-07-18 12:14:28 +0530

    coppa enabled added

--- integration-repository/sql/wap_site_uac_fun.sql
+++ integration-repository/sql/wap_site_uac_fun.sql
@@ -1,21 +1,23 @@
-CREATE OR REPLACE FUNCTION wap_site_uac_fun(last_updated timestamp without time zone)
-RETURNS SETOF wap_site_uac_type AS
+CREATE OR REPLACE FUNCTION wap_site_uac_fun_coppa(last_updated timestamp without time zone)
+RETURNS SETOF wap_site_uac_type_coppa AS
 $BODY$
 DECLARE
-    row1    wap_site_uac_type%ROWTYPE;
+    row1    wap_site_uac_type_coppa%ROWTYPE;
 BEGIN
     FOR row1 IN
         SELECT  
-        	id,
-    		site_type_id,
-    		content_rating,
-    		app_type,
-    		categories,
-    		uac_mod_on from wap_site_uac
-    		where content_rating != '' or app_type !='' or categories != ''
-    		or content_rating is not null or app_type is not null or categories is not null
-    		or coppa_enabled != false
-    		and modified_on >=last_updated
+				id,       
+				market_id,             
+    			site_type_id,           
+    			content_rating,          
+    			app_type,              
+    			categories,              
+    			coppa_enabled,          
+    			modified_on from wap_site_uac
+    			where content_rating != '' or app_type !='' or categories != ''
+    			or content_rating is not null or app_type is not null or categories is not null
+    			or coppa_enabled != false
+    			and modified_on >=last_updated
  LOOP
         RETURN NEXT row1;
     END LOOP;
--- integration-repository/sql/wap_site_uac_type.sql
+++ integration-repository/sql/wap_site_uac_type.sql
@@ -1,6 +1,7 @@
-CREATE OR REPLACE TYPE wap_site_uac_type AS
+CREATE OR REPLACE TYPE wap_site_uac_type_coppa AS
 (
     id                      character varying(255),
+    market_id               character varying(255),
     site_type_id            bigint,
     content_rating          character varying(255),
     app_type                character varying(255),
@@ -8,4 +9,4 @@ CREATE OR REPLACE TYPE wap_site_uac_type AS
     coppa_enabled           boolean,
     modified_on             TIMESTAMP WITHOUT TIME ZONE
 );
-ALTER TYPE wap_site_uac_type OWNER TO postgres;
+ALTER TYPE wap_site_uac_type_coppa OWNER TO postgres;
