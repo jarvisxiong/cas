16e7e33c9155245f64f97d6914d7bd1894ce23d2
dhanacs <dhanasekarancs@gmail.com>
2014-07-24 16:09:38 +0530

    Test cases for APP and WAP.

--- integration-adnetworks/src/test/java/com/inmobi/adserve/channels/adnetworks/DCPMiaozhenAdnetworkTest.java
+++ integration-adnetworks/src/test/java/com/inmobi/adserve/channels/adnetworks/DCPMiaozhenAdnetworkTest.java
@@ -137,8 +137,9 @@ public class DCPMiaozhenAdnetworkTest extends TestCase {
         assertTrue(dcpMiaozhenAdNetwork.configureParameters(sasParams, casInternalRequestParameters, entity, null, null));
     }
 
+    // Test request URI for APP.
     @Test
-    public void testDCPMiaozhenRequestUri() throws Exception {
+    public void testDCPMiaozhenRequestUriAPP() throws Exception {
         SASRequestParameters sasParams = new SASRequestParameters();
         CasInternalRequestParameters casInternalRequestParameters = new CasInternalRequestParameters();
 
@@ -154,7 +155,7 @@ public class DCPMiaozhenAdnetworkTest extends TestCase {
         sasParams.setSource("APP");
         sasParams.setOsId(SASRequestParameters.HandSetOS.iOS.getValue());
         sasParams.setSlot((short) 10);
-        sasParams.setSiteIncId(12314321432L);
+        sasParams.setSiteIncId(1000000007L);
 
         String externalKey = "4246";
 
@@ -169,11 +170,54 @@ public class DCPMiaozhenAdnetworkTest extends TestCase {
 
         dcpMiaozhenAdNetwork.configureParameters(sasParams, casInternalRequestParameters, entity, null, beaconUrl);
 
-        String blindedSiteId = (new UUID(1234L, 12314321432L)).toString();
+        String blindedSiteId = (new UUID(1000000009L, 1000000007L)).toString();
         String actualUrl = dcpMiaozhenAdNetwork.getRequestUri().toString();
 
         // Compare the expected URL with actual URL after eliminating its last parameter.
-        String expectedUrl = "http://s.x.cn.miaozhen.com/bx?v=0&m_app=com.inmobi-exchange.00000000-0000-0000-0000-0002ddfda218&m_ua=Mozilla%2F5.0+%28compatible%3B+MSIE+9.0%3B+Windows+NT+6.1%3B+Trident%2F5.0%29&m_ip=206.29.182.240&m_adw=300&m_adh=250&m_pos=37.4429%2C-122.1514&m_net=2&m_os=iOS&m0=202cb962ac59075b964b07152d234b70&m5=23e2ewq445545&m_int=1&l=4246";
+        String expectedUrl = "http://s.x.cn.miaozhen.com/bx?v=0&m_app=com.inmobi-exchange.00000000-0000-0000-0000-00003b9aca07&m_ua=Mozilla%2F5.0+%28compatible%3B+MSIE+9.0%3B+Windows+NT+6.1%3B+Trident%2F5.0%29&m_ip=206.29.182.240&m_adw=300&m_adh=250&m_pos=37.4429%2C-122.1514&m_net=2&m_os=iOS&m0=202cb962ac59075b964b07152d234b70&m5=23e2ewq445545&m_int=1&l=4246";
+        actualUrl = actualUrl.substring(0, actualUrl.lastIndexOf('&'));
+
+        assertEquals(expectedUrl, actualUrl);
+    }
+
+    // Test request URI for WAP.
+    @Test
+    public void testDCPMiaozhenRequestUriWAP() throws Exception {
+        SASRequestParameters sasParams = new SASRequestParameters();
+        CasInternalRequestParameters casInternalRequestParameters = new CasInternalRequestParameters();
+
+        casInternalRequestParameters.blockedCategories = new ArrayList<Long>(Arrays.asList(new Long[] {50l, 51l}));
+        casInternalRequestParameters.latLong = "37.4429,-122.1514";
+        casInternalRequestParameters.uid = "202cb962ac59075b964b07152d234b70";
+        casInternalRequestParameters.uidIFA = "23e2ewq445545";
+        casInternalRequestParameters.uidADT = "0";
+        casInternalRequestParameters.uidIDUS1 = "202cb962ac59075b964b07152d234b70";
+
+        sasParams.setRemoteHostIp("206.29.182.240");
+        sasParams.setUserAgent("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
+        sasParams.setSource("WAP");
+        sasParams.setOsId(SASRequestParameters.HandSetOS.iOS.getValue());
+        sasParams.setSlot((short) 10);
+        sasParams.setSiteIncId(1000000007L);
+
+        String externalKey = "4246";
+
+        ChannelSegmentEntity entity = new ChannelSegmentEntity(AdNetworksTest.getChannelSegmentEntityBuilder(
+            miaozhenAdvId, "adgroupid", null, null, 0, null, null, true, true, externalKey, null, null, null, 0, true, null,
+            null, 0, null, false, false, false, false, false, false, false, false, false, false, null,
+            new ArrayList<Integer>(), 0.0d, null, null, 0));
+
+        String beaconUrl = "http://c2.w.inmobi.com/c.asm/4/b/bx5/yaz/2/b/a5/m/0/0/0"
+            + "/202cb962ac59075b964b07152d234b70/4f8d98e2-4bbd-40bc-87e5-22da170600f9/-1/1"
+            + "/9cddca11?beacon=true";
+
+        dcpMiaozhenAdNetwork.configureParameters(sasParams, casInternalRequestParameters, entity, null, beaconUrl);
+
+        String blindedSiteId = (new UUID(1000000009L, 1000000007L)).toString();
+        String actualUrl = dcpMiaozhenAdNetwork.getRequestUri().toString();
+
+        // Compare the expected URL with actual URL after eliminating its last parameter.
+        String expectedUrl = "http://s.x.cn.miaozhen.com/bx?v=0&m_ua=Mozilla%2F5.0+%28compatible%3B+MSIE+9.0%3B+Windows+NT+6.1%3B+Trident%2F5.0%29&m_ip=206.29.182.240&m_adw=300&m_adh=250&m_pos=37.4429%2C-122.1514&m_net=2&m_os=iOS&m0=202cb962ac59075b964b07152d234b70&m5=23e2ewq445545&m_int=1&l=4246";
         actualUrl = actualUrl.substring(0, actualUrl.lastIndexOf('&'));
 
         assertEquals(expectedUrl, actualUrl);
