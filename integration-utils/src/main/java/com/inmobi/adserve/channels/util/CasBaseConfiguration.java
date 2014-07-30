a220da4eed2a88b131b4a9eb05f600d5aa11650e
rajashekhar.c <rajashekhar.c@inmobi.com>
2014-07-30 18:23:48 +0530

    The comma seperated values couldn't be read into config correctly. This is the fix for it.

--- integration-utils/src/main/java/com/inmobi/adserve/channels/util/CasBaseConfiguration.java
+++ integration-utils/src/main/java/com/inmobi/adserve/channels/util/CasBaseConfiguration.java
@@ -1,35 +1,40 @@
 package com.inmobi.adserve.channels.util;
 
 import java.net.URL;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang.NotImplementedException;
 
 import com.google.common.collect.Maps;
 
 /**
- * This class was created because original getProperity in parent method has a synchronized block in it, which was causing performance bottlenecks at high cpu utilization. 
- * In this implementation we store all the values in our local map and the gets happen via the local map
+ * This class was created because original getProperity in parent method has a
+ * synchronized block in it, which was causing performance bottlenecks at high
+ * cpu utilization. In this implementation we store all the values in our local
+ * map and the gets happen via the local map
+ * 
  * @author rajashekhar.c
- *
+ * 
  */
 public class CasBaseConfiguration extends PropertiesConfiguration {
 
-	
 	final static private Object lock = new Object();
 
-	final static private Map<String, Object> map  = Maps.newHashMap();
+	final static private Map<String, Object> map = Maps.newHashMap();
 
-	
 	public CasBaseConfiguration(String configFile) throws ConfigurationException {
 		super(configFile);
+		cloneMap();
 	}
 
 	public CasBaseConfiguration(URL resource) throws ConfigurationException {
 		super(resource);
+		cloneMap();
 	}
-	
+
 	@Override
 	public void setProperty(String key, Object value) {
 		synchronized (lock) {
@@ -41,15 +46,19 @@ public class CasBaseConfiguration extends PropertiesConfiguration {
 	@Override
 	public Object getProperty(String key) {
 		if (map.get(key) == null) {
-			synchronized (lock) {
-				if (map.get(key) == null) {
-					map.put(key, super.getProperty(key));
-				}
-			}
+			return super.getProperty(key);
 		}
 		return map.get(key);
 	}
 
+	private void cloneMap() {
+		Iterator keys = getKeys();
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+			Object value = super.getProperty(key);
+			map.put(key, value);
+		}
+	}
 
 	@Override
 	public void clearProperty(String key) {
@@ -58,12 +67,9 @@ public class CasBaseConfiguration extends PropertiesConfiguration {
 			map.remove(key);
 		}
 	}
-	
+
 	@Override
-	public void clear(){
-		synchronized (lock) {
-			super.clear();
-			map.clear();
-		}
+	public void clear() {
+		throw new NotImplementedException();
 	}
 }
