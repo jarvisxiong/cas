0bba56feb73d011983567a002be772e1c19aff97
Anshul Soni <anshul.soni@InMobis-MacBook-Pro-10.local>
2014-08-19 15:44:12 +0530

    making config location configurable for cas, add it as -DconfigFile=sampleConfigFile while starting the jar

--- Server/src/main/java/com/inmobi/adserve/channels/server/ChannelServer.java
+++ Server/src/main/java/com/inmobi/adserve/channels/server/ChannelServer.java
@@ -68,7 +68,7 @@ import com.netflix.governator.lifecycle.LifecycleManager;
 /*
  * 
  * Run jar:- java -Dincoming.connections=100 -Ddcpoutbound.connections=50 -Drtboutbound.connections=50 -DconfigFile=sampleConfigFile -jar
- * cas-server.jar If these are not specified server will pick these values from channel-server.properties config file. If configFile is not specified, it takes the DEFAULTCONFIGFILE from location "/opt/mkhoj/conf/cas/channel-server.properties"
+ * cas-server.jar If these are not specified server will pick these values from channel-server.properties config file. If configFile is not specified, it takes the DEFAULT_CONFIG_FILE from location "/opt/mkhoj/conf/cas/channel-server.properties"
  */
 public class ChannelServer {
     private static Logger                           logger;
@@ -86,14 +86,14 @@ public class ChannelServer {
     private static WapSiteUACRepository             wapSiteUACRepository;
     private static CreativeRepository               creativeRepository;
     private static NativeAdTemplateRepository		nativeAdTemplateRepository;
-    private static final String                     DEFAULTCONFIGFILE="/opt/mkhoj/conf/cas/channel-server.properties";
+    private static final String                     DEFAULT_CONFIG_FILE="/opt/mkhoj/conf/cas/channel-server.properties";
     private static String                           configFile;
     public static byte                              dataCenterIdCode;
     public static short                             hostIdCode;
     public static String                            dataCentreName;
 
     public static void main(final String[] args) throws Exception {
-        configFile=System.getProperty("configFile",DEFAULTCONFIGFILE);
+        configFile=System.getProperty("configFile",DEFAULT_CONFIG_FILE);
         try {
             ConfigurationLoader configurationLoader = ConfigurationLoader.getInstance(configFile);
 
