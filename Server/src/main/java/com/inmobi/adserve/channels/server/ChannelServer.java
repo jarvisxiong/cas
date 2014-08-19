9350a8767c4c315ab4ef137ce80e359d61cf68e5
Anshul Soni <anshul.soni@InMobis-MacBook-Pro-10.local>
2014-08-19 11:56:11 +0530

    Making config location configurable for cas, add it as -DconfigFile=sampleConfigFile while running the jar

--- Server/src/main/java/com/inmobi/adserve/channels/server/ChannelServer.java
+++ Server/src/main/java/com/inmobi/adserve/channels/server/ChannelServer.java
@@ -93,6 +93,7 @@ public class ChannelServer {
 
     public static void main(final String[] args) throws Exception {
         configFile=System.getProperty("configFile","/opt/mkhoj/conf/cas/channel-server.properties");
+	System.out.println(configFile);
         try {
             ConfigurationLoader configurationLoader = ConfigurationLoader.getInstance(configFile);
 
