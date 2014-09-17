8ce1e03c4b4ab260bddf93458e0dce4b13ddc93a
rajashekhar.c <rajashekhar.c@inmobi.com>
2014-09-17 13:02:07 +0530

    Cleaning up necessary methods

--- integration-utils/src/main/java/com/inmobi/adserve/channels/util/InspectorStats.java
+++ integration-utils/src/main/java/com/inmobi/adserve/channels/util/InspectorStats.java
@@ -6,9 +6,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.json.JSONObject;
 
+import com.yammer.metrics.core.Counter;
+
 
 public class InspectorStats {
     private static Map<String, ConcurrentHashMap<String, ConcurrentHashMap<String, AtomicLong>>> stats = new ConcurrentHashMap<String, ConcurrentHashMap<String, ConcurrentHashMap<String, AtomicLong>>>();
+    private static Map<String, ConcurrentHashMap<String, ConcurrentHashMap<String, Counter>>> yammerStats = new ConcurrentHashMap<String, ConcurrentHashMap<String, ConcurrentHashMap<String, Counter>>>();
 
     public static void incrementStatCount(final String parameter, final long value) {
         incrementStatCount("WorkFlow", parameter, value);
@@ -40,34 +43,8 @@ public class InspectorStats {
         }
     }
 
-    public static void setStats(final String parameter, final long value) {
-        setStats("WorkFlow", parameter, value);
-    }
-
-    public static void setStats(final String key, final String parameter, final long value) {
-        if (stats.get(key) == null) {
-            stats.put(key, new ConcurrentHashMap<String, ConcurrentHashMap<String, AtomicLong>>());
-            stats.get(key).put("stats", new ConcurrentHashMap<String, AtomicLong>());
-            stats.get(key).get("stats").put(parameter, new AtomicLong(value));
-        }
-        else if (stats.get(key).get("stats") == null) {
-            stats.get(key).put("stats", new ConcurrentHashMap<String, AtomicLong>());
-            stats.get(key).get("stats").put(parameter, new AtomicLong(value));
-        }
-        else {
-            stats.get(key).get("stats").put(parameter, new AtomicLong(value));
-        }
-    }
-
-    public static void setWorkflowStats(final String parameter, final long value) {
-        setStats("WorkFlow", parameter, value);
-    }
-
     public static String getStats() {
         return (new JSONObject(stats).toString());
     }
 
-    public static void initializeSiteFeedbackStats() {
-        stats.put("SiteFeedback", new ConcurrentHashMap<String, ConcurrentHashMap<String, AtomicLong>>());
-    }
 }
