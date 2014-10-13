7b7243ca5b94a145d3e3663887130b572d35e168
Yasir Imteyaz <yasir.imteyaz@inmobi.com>
2014-10-13 16:22:22 +0530

    Revert "moving dns lookup property setting to static block"
    This reverts commit a9ea834e15f21b1bad6edbd0b13e3c810ed3df50.

--- Server/src/main/java/com/inmobi/adserve/channels/server/ChannelServer.java
+++ Server/src/main/java/com/inmobi/adserve/channels/server/ChannelServer.java
@@ -93,12 +93,6 @@ public class ChannelServer {
     public static byte                              dataCenterIdCode;
     public static short                             hostIdCode;
     public static String                            dataCentreName;
-    
-    static {
-      // Increase networkaddress cache ttl to avoid thread wait. (SERVOPS-3265)
-      java.security.Security.setProperty("networkaddress.cache.ttl","3600");
-      java.security.Security.setProperty("networkaddress.cache.negative.ttl", "60");
-    }
 
     public static void main(final String[] args) throws Exception {
         configFile=System.getProperty("configFile",DEFAULT_CONFIG_FILE);
@@ -137,6 +131,13 @@ public class ChannelServer {
 
             // Initialising ClickUrlsRegenerator
             ClickUrlsRegenerator.init(configurationLoader.getServerConfiguration().subset("clickmaker"));
+
+            // Increase networkaddress cache ttl to avoid thread wait. (SERVOPS-3265)
+            java.security.Security.setProperty("networkaddress.cache.ttl",
+                    configurationLoader.getServerConfiguration().getString("networkaddress.cache.ttl", "3600"));
+
+            java.security.Security.setProperty("networkaddress.cache.negative.ttl",
+                    configurationLoader.getServerConfiguration().getString("networkaddress.cache.negative.ttl", "300"));
 
             String rrLogKey = configurationLoader.getServerConfiguration().getString("rrLogKey");
             String advertisementLogKey = configurationLoader.getServerConfiguration().getString("adsLogKey");
