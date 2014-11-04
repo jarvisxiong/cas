e141a474ec9a0ba8e66aadf9e680a9b391ed9366
ishanbhatnagar <ishan.bhatnagar@inmobi.com>
2014-11-04 18:40:26 +0530

    Version 2 of servlet

--- Server/src/main/java/com/inmobi/adserve/channels/server/servlet/ServletChangeLogLevel.java
+++ Server/src/main/java/com/inmobi/adserve/channels/server/servlet/ServletChangeLogLevel.java
@@ -1,17 +1,11 @@
 package com.inmobi.adserve.channels.server.servlet;
 
-/**
- * Created by ishanbhatnagar on 31/10/14.
- */
-
 import com.google.inject.Singleton;
 import com.inmobi.adserve.channels.server.HttpRequestHandler;
 import com.inmobi.adserve.channels.server.api.Servlet;
 import com.inmobi.adserve.channels.util.Utils.TestUtils;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.QueryStringDecoder;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,81 +13,80 @@ import javax.ws.rs.Path;
 import java.util.List;
 import java.util.Map;
 
-// Work in progress
+/**
+ * Created by ishanbhatnagar on 31/10/14.
+ */
 
 @Singleton
 @Path("/changeLogLevel")
 public class ServletChangeLogLevel implements Servlet {
     private static final Logger LOG = LoggerFactory.getLogger(ServletChangeLogLevel.class);
+    private static final String invalidLoggerName =
+            "Invalid Logger Name or Incorrect Query Format.\n"
+            + "Valid logger names are: debug, advertiser, sampledadvertiser\n"
+            + "Valid Query Format: " + TestUtils.SampleServletQueries.servletChangeLogLevel;
+    private static final String invalidLoggerLevel =
+            "Invalid Logger Level. Valid logger levels are: DEBUG, INFO, WARN, ERROR, OFF";
 
-    @Override
     public void handleRequest(final HttpRequestHandler hrh, final QueryStringDecoder queryStringDecoder,
                               final Channel serverChannel) throws Exception {
         LOG.error("Inside Change Log Level Servlet");
 
-        final Map<String, List<String>> params = queryStringDecoder.parameters();
-        final String requestParam = params.get("args").toString();
-        final JSONArray jsonArray = new JSONArray(requestParam);
-        final JSONObject jObject = jsonArray.getJSONObject(0);
-
-        String failedResponse = "Incorrect Query Format. Here's an example: " + TestUtils.SampleServletQueries.servletChangeLogLevel;
-        String successfulResponse = "Successfully changed logging level.";
-
         try {
-            String appenderName = jObject.getString("appender");
-            String levelName    = jObject.getString("level");
+            final Map<String, List<String>> params = queryStringDecoder.parameters();
+            String loggerName = null;
+            String levelName = null;
 
-            ch.qos.logback.classic.Level level = getLogger(levelName);
+            if (params.containsKey("debug")) {
+                loggerName = "com.inmobi.adserve.channels";
+                levelName = params.get("debug").get(0).toString();
+            } else if (params.containsKey("advertiser")) {
+                loggerName = "advertiser";
+                levelName = params.get("advertiser").get(0).toString();
+            } else if (params.containsKey("sampledadvertiser")) {
+                loggerName = "sampledadvertiser";
+                levelName = params.get("sampledadvertiser").get(0).toString();
+            } else {
+                LOG.error(invalidLoggerName);
+                hrh.responseSender.sendResponse(invalidLoggerName, serverChannel);
+                return;
+            }
+
+            ch.qos.logback.classic.Level level = getLevel(levelName);
             if (null == level) {
-                LOG.error("Invalid Logger Level");
-                hrh.responseSender.sendResponse(failedResponse, serverChannel);
-            }
-            if (false == isValidAppender(appenderName)) {
-                LOG.error("No such Appender Name");
-                hrh.responseSender.sendResponse(failedResponse, serverChannel);
+                LOG.error(invalidLoggerLevel);
+                hrh.responseSender.sendResponse(invalidLoggerLevel, serverChannel);
+                return;
             }
 
-            //((ch.qos.logback.classic.Logger)LoggerFactory.getLogger(ServletChangeLogLevel.class)).setLevel(ch.qos.logback.classic.Level.ERROR);
-            ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).setLevel(level);
-            /*LogManager.getRootLogger().setLevel(level);
-            Enumeration allLoggers = LogManager.getLoggerRepository().getCurrentLoggers();
-            while(allLoggers.hasMoreElements()) {
-                ((org.apache.log4j.Logger)allLoggers.nextElement()).setLevel(level);
-            }*/
-            for(ch.qos.logback.classic.Logger logger: ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).getLoggerContext().getLoggerList()) {
-                logger.setLevel(level);
-            }
+            ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger(loggerName)).setLevel(level);
 
-            LOG.debug("DEBUG is on");
-            LOG.info("INFO is on");
-            LOG.error("ERROR is on");
-
-            LOG.error("Successfully changed log level of {} to {}", appenderName, levelName);
-            hrh.responseSender.sendResponse(successfulResponse, serverChannel);
+            String successMessage = "Successfully changed log level of " + loggerName
+                    + " and all it's descendants to " + levelName;
+            LOG.error(successMessage);
+            hrh.responseSender.sendResponse(successMessage, serverChannel);
         }
         catch (Exception e) {
-
+            LOG.error("Exception caught: {}", e.toString());
+            hrh.responseSender.sendResponse(e.toString(), serverChannel);
+            return;
         }
     }
 
     @Override
     public String getName() {
-        return "lbstatus";
+        return "changeLogLevel";
     }
 
-    private ch.qos.logback.classic.Level getLogger(String sArg) {
+    private ch.qos.logback.classic.Level getLevel(String sArg) {
         ch.qos.logback.classic.Level level = null;
         switch(sArg) {
-            case "OFF": level = ch.qos.logback.classic.Level.OFF;   break;
             case "DEBUG": level = ch.qos.logback.classic.Level.DEBUG;   break;
-            case "WARN": level = ch.qos.logback.classic.Level.WARN;   break;
+            case "WARN":  level = ch.qos.logback.classic.Level.WARN;    break;
+            case "INFO":  level = ch.qos.logback.classic.Level.INFO;    break;
             case "ERROR": level = ch.qos.logback.classic.Level.ERROR;   break;
-            default: level = null;
+            case "OFF":   level = ch.qos.logback.classic.Level.OFF;     break;
         }
         return level;
-    }
-
-    private Boolean isValidAppender(String appender) {
-        return true;
     }
 }
