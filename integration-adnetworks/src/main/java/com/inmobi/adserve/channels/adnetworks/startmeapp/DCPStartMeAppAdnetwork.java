a35db05aa213957e0a62203b0c738c2601122bc4
Ritwik Kumar <ritwik.kumar@inmobi.com>
2015-04-16 18:34:28 +0530

    Cleanup

--- integration-adnetworks/src/main/java/com/inmobi/adserve/channels/adnetworks/startmeapp/DCPStartMeAppAdnetwork.java
+++ integration-adnetworks/src/main/java/com/inmobi/adserve/channels/adnetworks/startmeapp/DCPStartMeAppAdnetwork.java
@@ -9,9 +9,12 @@ import com.inmobi.adserve.channels.entity.SlotSizeMapEntity;
 import com.inmobi.adserve.channels.util.InspectorStats;
 import com.inmobi.adserve.channels.util.InspectorStrings;
 import com.inmobi.adserve.channels.util.VelocityTemplateFieldConstants;
+import com.inmobi.adserve.channels.util.config.GlobalConstant;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpResponseStatus;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.lang.StringUtils;
 import org.apache.velocity.VelocityContext;
@@ -27,7 +30,7 @@ import java.net.URISyntaxException;
 /**
  * Created by deepak on 27/3/15.
  */
-public class DCPStartMeAppAdnetwork extends AbstractDCPAdNetworkImpl{
+public class DCPStartMeAppAdnetwork extends AbstractDCPAdNetworkImpl {
     private static final Logger LOG = LoggerFactory.getLogger(DCPStartMeAppAdnetwork.class);
     private static final String APIVER = "apiver";
     private static final String PUBID = "pubid";
@@ -38,15 +41,15 @@ public class DCPStartMeAppAdnetwork extends AbstractDCPAdNetworkImpl{
     private static final String OSV = "osv";
     private static final String HEIGHT = "height";
     private static final String WIDTH = "width";
-    private static final String PDID= "pdid";
-    private static final String KEYWORDS= "keywords";
-    private static String os =null;
+    private static final String PDID = "pdid";
+    private static final String KEYWORDS = "keywords";
+    private static String os = null;
     private static String pubid;
     public static Integer height;
     public static Integer width;
 
     public DCPStartMeAppAdnetwork(final Configuration config, final Bootstrap clientBootstrap,
-                                  final HttpRequestHandlerBase baseRequestHandler, final Channel serverChannel) {
+            final HttpRequestHandlerBase baseRequestHandler, final Channel serverChannel) {
         super(config, clientBootstrap, baseRequestHandler, serverChannel);
     }
 
@@ -75,35 +78,36 @@ public class DCPStartMeAppAdnetwork extends AbstractDCPAdNetworkImpl{
     }
 
     @Override
-    public String getId() { return config.getString("startmeappdcp.advertiserId"); }
+    public String getId() {
+        return config.getString("startmeappdcp.advertiserId");
+    }
 
     @Override
     public URI getRequestUri() throws Exception {
         try {
             final StringBuilder url = new StringBuilder(host);
-            appendQueryParam(url,APIVER,"1", true);
-            appendQueryParam(url,ADSPACE, externalSiteId, false);
-            appendQueryParam(url,PUBID,pubid, false);
-            appendQueryParam(url,RESPONSE, "JSON", false);
-            appendQueryParam(url,SOURCE, sasParams.getSource(), false);
-            appendQueryParam(url,IP, sasParams.getRemoteHostIp(), false);
-            appendQueryParam(url,UA, getURLEncode(sasParams.getUserAgent(), format), false);
+            appendQueryParam(url, APIVER, GlobalConstant.ONE, true);
+            appendQueryParam(url, ADSPACE, externalSiteId, false);
+            appendQueryParam(url, PUBID, pubid, false);
+            appendQueryParam(url, RESPONSE, "JSON", false);
+            appendQueryParam(url, SOURCE, sasParams.getSource(), false);
+            appendQueryParam(url, IP, sasParams.getRemoteHostIp(), false);
+            appendQueryParam(url, UA, getURLEncode(sasParams.getUserAgent(), format), false);
             if (sasParams.getOsId() == SASRequestParameters.HandSetOS.iOS.getValue()) {
-                appendQueryParam(url,PDID, casInternalRequestParameters.getUidIFA(), false);}
-            else{
-                    appendQueryParam(url,PDID, casInternalRequestParameters.getGpid(), false);
+                appendQueryParam(url, PDID, casInternalRequestParameters.getUidIFA(), false);
+            } else {
+                appendQueryParam(url, PDID, casInternalRequestParameters.getGpid(), false);
             }
-            appendQueryParam(url,HEIGHT, height, false);
-            appendQueryParam(url,WIDTH, width, false);
-            appendQueryParam(url,KEYWORDS, getURLEncode(getCategories(','), format), false);
-            if(sasParams.getOsId() == SASRequestParameters.HandSetOS.iOS.getValue()){
-                os="IOS";
+            appendQueryParam(url, HEIGHT, height, false);
+            appendQueryParam(url, WIDTH, width, false);
+            appendQueryParam(url, KEYWORDS, getURLEncode(getCategories(','), format), false);
+            if (sasParams.getOsId() == SASRequestParameters.HandSetOS.iOS.getValue()) {
+                os = "IOS";
+            } else if (sasParams.getOsId() == SASRequestParameters.HandSetOS.Android.getValue()) {
+                os = "Android";
             }
-            else if(sasParams.getOsId() == SASRequestParameters.HandSetOS.Android.getValue()){
-                os="Android";
-            }
-            appendQueryParam(url,OS, os, false);
-            appendQueryParam(url,OSV, sasParams.getOsMajorVersion(), false);
+            appendQueryParam(url, OS, os, false);
+            appendQueryParam(url, OSV, sasParams.getOsMajorVersion(), false);
             LOG.debug("StartMeAppDCP url is {}", url);
             return new URI(url.toString());
         } catch (final URISyntaxException exception) {
@@ -127,14 +131,14 @@ public class DCPStartMeAppAdnetwork extends AbstractDCPAdNetworkImpl{
                 final VelocityContext context = new VelocityContext();
                 Formatter.TemplateType t = Formatter.TemplateType.HTML;
                 context.put(VelocityTemplateFieldConstants.PARTNER_HTML_CODE, adResponse.getString("adm"));
-                adStatus = "AD";
+                adStatus = AD_STRING;
                 responseContent = Formatter.getResponseFromTemplate(t, context, sasParams, beaconUrl);
-            }catch (final JSONException exception) {
-                adStatus = "NO_AD";
+            } catch (final JSONException exception) {
+                adStatus = NO_AD;
                 LOG.info("Error parsing response {} from Startmeappdcp: {}", response, exception);
                 InspectorStats.incrementStatCount(getName(), InspectorStrings.PARSE_RESPONSE_EXCEPTION);
-            }catch (final Exception exception) {
-                adStatus = "NO_AD";
+            } catch (final Exception exception) {
+                adStatus = NO_AD;
                 LOG.info("Error parsing response {} from Startmeappdcp: {}", response, exception);
                 InspectorStats.incrementStatCount(getName(), InspectorStrings.PARSE_RESPONSE_EXCEPTION);
             }
