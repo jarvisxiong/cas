544c5f123ead200ba2a33e5ca3790ce655fc49ab
avinash.kumar <avinash.kumar@inmobi.com>
2015-12-24 18:55:29 +0530

    changes

--- casautomation/src/main/java/com/inmobi/castest/utils/common/CasServerDetails.java
+++ casautomation/src/main/java/com/inmobi/castest/utils/common/CasServerDetails.java
@@ -9,7 +9,7 @@ public class CasServerDetails {
 
     private final static String CAS_SERVER_ENDPOINT = "http://" + CAS_SERVER_IP + ":8800/";
 
-    private final static String LOG_FILE_PATH = "/opt/mkhoj/logs/cas/debug/";
+    private final static String LOG_FILE_PATH = "/opt/inmobi/logs/cas/debug/";
 
     private final static String LOG_PARSER_URL = CAS_SERVER_ENDPOINT + "logParser";
 
